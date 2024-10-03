a21ab7a6f96a9da021d44db0c0054cab1d805663
Fadhli <fadhlijabbar@upi.edu>
2024-10-04 05:02:06 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'plusjakarta': ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      colors: {
+        'neutral-primary': '#333333',
+        'neutral-secondary': '#686868',
+        'neutral-border': '#DADADA',
+        'neutral-menu': '#f1f1f1',
+        'primary': '#3A6DF1',
+        'primary-active': '#3462D9',
+      },
+      width: {
+        '800': '800px',
+      },
+      rotate: {
+        '360': '360deg',
+      }
+    },
+  },
+  plugins: [],
+}
