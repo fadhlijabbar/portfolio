d0260c7edc239df944925d8dc49cc1c8559bcc1c
Fadhli <fadhlijabbar@upi.edu>
2024-10-17 05:13:21 +0700

    Some interactions

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,17 @@ module.exports = {
       },
       rotate: {
         '360': '360deg',
-      }
+      },
+      animation: {
+        'custom-ping': 'custom-ping 1s ease-in-out infinite',
+      },
+      keyframes: {
+        'custom-ping': {
+          '0%': { transform: 'scale(1)', opacity: '0.5' },
+          '50%': { transform: 'scale(1.5)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '0' },
+        },
+      },
     },
   },
   plugins: [],
